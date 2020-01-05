770f161da7127808fc44884f93a0b812607658e3
Martin Ogden <martin.ogden@gmail.com>
2020-01-05 23:47:06 +0000

    add heap allocation to runtime

--- runtime/l3rt.c
+++ runtime/l3rt.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define WORD_SIZE 8
+
 
 extern int _c0_main();
+
 /* The main function, which calls _c0_main */
 int main() {
-  printf("%d\n", _c0_main());
-  exit(0);
+	printf("%d\n", _c0_main());
+	exit(0);
 }
 
 
 void assert(int b) {
 	if (!b) abort();
+}
+
+
+void* _c0_alloc(size_t size) {
+	return malloc(size);
+}
+
+
+void* _c0_alloc_array(size_t size, size_t n) {
+	void* ptr = calloc(n+1, size);
+	*(int*)(ptr + size - WORD_SIZE) = n;
+	return ptr + size;
 }
